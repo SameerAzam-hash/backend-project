ea3f11c5005fd93b593fbb1c5f238f04e8e13ca3
SameerAzam-hash <awaisahmadjrw2000@gmail.com>
2025-02-05 21:26:33 +0500

    add changes in git

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,29 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiErrors.js";
+import { User } from "../models/user.model.js"
 
-import { asyncHandler } from "../utils/asyncHandler.js"
+const registerUser = asyncHandler(async (req, res) => {
+  // steps to make the backend code for registration of user in Frontend
+  //get user information from the frontend
+  // Validation for backend not user get any empty box like email or passsword - not empty
+  //check if user already exists: username, email
+  //check for images and avatar
+  //upload them to cloudinary, avatar
+  //create user object - create entry in db
+  //remove password and refresh tokenfield from response
+  //check for user crreation
+  //return response
 
-const registerUser = asyncHandler (async (req,res) => {
-    res.status(200).json({
-        message: "sameer is running"
-    })
-})
+  const { fullName, email, password, username } = req.body;
+  console.log("email :", email);
 
+  if (
+    [fullName, email, password, username].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "ALL fields are required");
+  }
+});
 
-export {registerUser};
+export {
+  registerUser
+};
